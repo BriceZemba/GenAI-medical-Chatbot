d28b6be6e7dee52cc68ea5f3c1e21dba430bfcf5
LeSecondEtre <bricezemba336@gmail.com>
2024-12-13 00:41:57 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'TheSecond',
+    author_email= 'bricezemba336@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
